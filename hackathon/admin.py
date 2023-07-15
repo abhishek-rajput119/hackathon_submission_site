ee8be09ee856712d60a115e02d4aafe4b023a43f
Abhishek Rajput <abrajput119@gmail.com>
2023-07-15 14:48:42 +0530

    registering apps to django-admin

--- hackathon/admin.py
+++ hackathon/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from .models import Profile
+from .models import Profile,Hackathon,Submission
 
 
 class CoderInline(admin.StackedInline):
@@ -20,3 +20,6 @@ admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 
 # Register your models here.
+admin.site.register(Hackathon)
+admin.site.register(Profile)
+admin.site.register(Submission)
