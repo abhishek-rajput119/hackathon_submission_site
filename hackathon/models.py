a9a8f7ce8c43fa973a09ab0fa1d0908aa695d9fa
Abhishek Rajput <abrajput119@gmail.com>
2023-07-15 13:49:05 +0530

    adding logic for login and signup

--- hackathon/models.py
+++ hackathon/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user.username
+
+class Hackathon(models.Model):
+    user_id = models.IntegerField()
+    title = models.CharField(max_length=40)
+    description = models.TextField(max_length= 200)
+    start_time = models.DateTimeField()
+    end_date = models.DateTimeField()
+    active = models.DateTimeField()
+    reward = models.PositiveIntegerField()
+    created = models.DateTimeField()
+    updated = models.DateTimeField()
+
+class Submission(models.Model):
+    IMAGE = 0
+    FILE = 1
+    LINK = 2
+
+    TYPE_OF_SUBMISSION = (
+        (IMAGE, "image"),
+        (FILE, "file"),
+        (LINK, "link"),
+    )
+    hackathon_id = models.IntegerField()
+    summary = models.TextField()
+    type_of_submission = models.PositiveIntegerField(choices=TYPE_OF_SUBMISSION)
+
